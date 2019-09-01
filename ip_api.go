dbf203b75783a46732035a6d2c3866c2af6ca38c
BenB196 <benbrenek@yahoo.com>
2019-09-01 12:17:32 -0400

    Added functions for actually quering ip api

--- ip_api.go
+++ ip_api.go
@@ -1,5 +1,14 @@
 package ip_api
 
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
 //URI for the free IP-API
 const FreeAPIURI = "http://ip-api.com/"
 
@@ -38,6 +47,176 @@ type Query struct {
 	Lang	string		`json:"lang,omitempty"`
 }
 
-//TODO func for single query /json/
+//Execute a single query (queries field should only contain 1 value
+func SingleQuery(query Query, apiKey string) (Location, error) {
+	//Make sure that there is only 1 query value
+	if len(query.Queries) != 1 {
+		return Location{}, errors.New("error: only 1 query can be passed to single query api")
+	}
 
-//TODO func for batch query /batch
+	//Build URI
+	uri := buildURI(query, "single",apiKey)
+
+	//Execute query
+	req, err := http.NewRequest("GET",uri,nil)
+
+	if err != nil {
+		return Location{}, err
+	}
+
+	//Set request headers
+	req.Header.Set("Accept","application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		return Location{}, err
+	}
+
+	defer resp.Body.Close()
+
+	//Check if invalid api key
+	if resp.StatusCode == 403 {
+		if strings.Contains(uri, "?key=") {
+			return Location{}, errors.New("error: invalid api key")
+		} else {
+			return Location{}, errors.New("error: exceeded api calls per minute, you need to un-blacklist yourself")
+		}
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return Location{}, errors.New("error querying ip api: " + resp.Status + " " + strconv.Itoa(resp.StatusCode))
+	}
+
+	var location Location
+
+	err = json.NewDecoder(resp.Body).Decode(&location)
+
+	if err != nil {
+		return Location{}, err
+	}
+
+	return location,nil
+}
+
+//Execute a batch query (queries field should contain 1 or more values
+func BatchQuery(query Query, apiKey string) ([]Location, error) {
+	//Make sure that there are 1 or more query values
+	if len(query.Queries) < 1 {
+		return nil, errors.New("error: no queries passed to batch query")
+	}
+
+	//Build URI
+	uri := buildURI(query,"batch",apiKey)
+
+	//Build queries list
+	queries, err := json.Marshal(query.Queries)
+
+	if err != nil {
+		return nil, err
+	}
+
+	//Execute Query
+	req, err := http.NewRequest("POST",uri,bytes.NewReader(queries))
+
+	if err != nil {
+		return nil, err
+	}
+
+	//Set request headers
+	req.Header.Set("Content-Type","application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	//Check if invalid api key
+	if resp.StatusCode == 403 {
+		if strings.Contains(uri, "?key=") {
+			return nil, errors.New("error: invalid api key")
+		} else {
+			return nil, errors.New("error: exceeded api calls per minute, you need to un-blacklist yourself")
+		}
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("error querying ip api: " + resp.Status + " " + strconv.Itoa(resp.StatusCode))
+	}
+
+	var locations []Location
+
+	err = json.NewDecoder(resp.Body).Decode(&locations)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return locations,nil
+}
+
+func buildURI(query Query, queryType string, apiKey string) string {
+	var baseURI string
+	//Set base URI
+	switch apiKey {
+	case "":
+		baseURI = FreeAPIURI
+	default:
+		baseURI = FreeAPIURI
+	}
+
+	//Update base URI with query type
+	switch queryType {
+	case "single":
+		baseURI = baseURI + "json/" + query.Queries[0]
+	case "batch":
+		baseURI = baseURI + "batch"
+	}
+
+	//Get fields list if fields len > 0
+	var fieldsList string
+	if len(query.Fields) > 0 {
+		fieldsList = buildFieldList(query.Fields)
+	}
+
+	//Get lang string if lang != ""
+	var lang string
+	if query.Lang != "" {
+		lang = buildLangString(query.Lang)
+	}
+
+	//Update base URI with api key if not ""
+	switch apiKey {
+	case "":
+		if fieldsList != "" && lang != "" {
+			baseURI = baseURI + "?" + fieldsList + "&" + lang
+		} else if fieldsList != "" {
+			baseURI = baseURI + "?" + fieldsList
+		} else if lang != "" {
+			baseURI = baseURI + "?" + lang
+		}
+	default:
+		baseURI = baseURI + "?key=" + apiKey
+		if fieldsList != "" && lang != "" {
+			baseURI = baseURI + "&" + fieldsList + "&" + lang
+		} else if fieldsList != "" {
+			baseURI = baseURI + "&" + fieldsList
+		} else if lang != "" {
+			baseURI = baseURI + "&" + lang
+		}
+	}
+	return baseURI
+}
+
+//Build fields string from slice
+func buildFieldList(fields []string) string {
+	return "fields=" + strings.Join(fields,",")
+}
+
+//Build lang string from lang value
+func buildLangString(lang string) string {
+	return "lang=" + lang
+}
