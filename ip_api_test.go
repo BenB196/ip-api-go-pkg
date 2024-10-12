9184efc737429e830337c46a1914060ade9e7f50
Ben <benbrenek@yahoo.com>
2024-10-12 12:10:09 -0400

    refactor: pkg tests and update to support new field

--- ip_api_test.go
+++ ip_api_test.go
@@ -2,119 +2,190 @@ package ip_api
 
 import (
 	"encoding/json"
-	"log"
 	"testing"
 )
 
-func getSuccessfulSingleResponse() string {
-	var lat float32 = 40.7357
-	var lon float32 = -74.1724
-	var mobile = false
-	var proxy = false
-	var hosting = true
+const allFieldCsv string = "status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,query,hosting"
 
-	location := Location{
+var googleAllFieldsDefaultLangRequestPointers = struct {
+	lat     float32
+	lon     float32
+	offset  int
+	mobile  bool
+	proxy   bool
+	hosting bool
+}{
+	lat:     39.03,
+	lon:     -77.5,
+	offset:  -14400,
+	mobile:  false,
+	proxy:   false,
+	hosting: true,
+}
+
+var googleAllFieldsDefaultLangRequestTest = struct {
+	query struct {
+		name  string
+		query Query
+	}
+	out struct {
+		Location
+		err error
+	}
+}{
+	query: struct {
+		name  string
+		query Query
+	}{
+		name: "googleAllFieldsDefaultLang",
+		query: Query{
+			Queries: []QueryIP{
+				{Query: "8.8.8.8"},
+			},
+			Fields: allFieldCsv,
+			Lang:   "",
+		},
+	},
+	out: struct {
+		Location
+		err error
+	}{Location: Location{
 		Status:        "success",
 		Message:       "",
 		Continent:     "North America",
 		ContinentCode: "NA",
 		Country:       "United States",
 		CountryCode:   "US",
-		Region:        "NJ",
-		RegionName:    "New Jersey",
-		City:          "Newark",
+		Region:        "VA",
+		RegionName:    "Virginia",
+		City:          "Ashburn",
 		District:      "",
-		ZIP:           "07175",
-		Lat:           &lat,
-		Lon:           &lon,
+		ZIP:           "20149",
+		Lat:           &googleAllFieldsDefaultLangRequestPointers.lat,
+		Lon:           &googleAllFieldsDefaultLangRequestPointers.lon,
 		Timezone:      "America/New_York",
-		Currency:      "",
+		Offset:        &googleAllFieldsDefaultLangRequestPointers.offset,
+		Currency:      "USD",
 		ISP:           "Google LLC",
-		Org:           "Level 3",
+		Org:           "Google Public DNS",
 		AS:            "AS15169 Google LLC",
 		ASName:        "GOOGLE",
 		Reverse:       "dns.google",
-		Mobile:        &mobile,
-		Proxy:         &proxy,
-		Hosting:       &hosting,
+		Mobile:        &googleAllFieldsDefaultLangRequestPointers.mobile,
+		Proxy:         &googleAllFieldsDefaultLangRequestPointers.proxy,
+		Hosting:       &googleAllFieldsDefaultLangRequestPointers.hosting,
 		Query:         "8.8.8.8",
-	}
-	
-	result, _ := json.Marshal(location)
-	
-	return string(result)
+	},
+		err: nil,
+	},
 }
 
-func getSuccessfulBatchResponse() string {
-	var lat1 float32 = 40.7357
-	var lon1 float32 = -74.1724
-	var mobile1 = false
-	var proxy1 = false
-	var hosting1 = true
+var cloudflareAllFieldsDefaultLangRequestPointers = struct {
+	lat     float32
+	lon     float32
+	offset  int
+	mobile  bool
+	proxy   bool
+	hosting bool
+}{
+	lat:     -27.4766,
+	lon:     153.0166,
+	offset:  36000,
+	mobile:  false,
+	proxy:   false,
+	hosting: true,
+}
 
-	location1 := Location{
-		Status:        "success",
-		Message:       "",
-		Continent:     "North America",
-		ContinentCode: "NA",
-		Country:       "United States",
-		CountryCode:   "US",
-		Region:        "NJ",
-		RegionName:    "New Jersey",
-		City:          "Newark",
-		District:      "",
-		ZIP:           "07175",
-		Lat:           &lat1,
-		Lon:           &lon1,
-		Timezone:      "America/New_York",
-		Currency:      "",
-		ISP:           "Google LLC",
-		Org:           "Level 3",
-		AS:            "AS15169 Google LLC",
-		ASName:        "GOOGLE",
-		Reverse:       "",
-		Mobile:        &mobile1,
-		Proxy:         &proxy1,
-		Hosting:       &hosting1,
-		Query:         "8.8.8.8",
+var cloudflareAllFieldsDefaultLangRequestTest = struct {
+	query struct {
+		name  string
+		query Query
 	}
-
-	var lat2 float32 = -33.8688
-	var lon2 float32 = 151.209
-	var mobile2 = false
-	var proxy2 = false
-	var hosting2 = true
-	
-	location2 := Location{
+	out struct {
+		Location
+		err error
+	}
+}{
+	query: struct {
+		name  string
+		query Query
+	}{
+		name: "cloudflareAllFieldsDefaultLang",
+		query: Query{
+			Queries: []QueryIP{
+				{Query: "1.1.1.1"},
+			},
+			Fields: allFieldCsv,
+			Lang:   "",
+		},
+	},
+	out: struct {
+		Location
+		err error
+	}{Location: Location{
 		Status:        "success",
 		Message:       "",
 		Continent:     "Oceania",
 		ContinentCode: "OC",
 		Country:       "Australia",
 		CountryCode:   "AU",
-		Region:        "NSW",
-		RegionName:    "New South Wales",
-		City:          "Sydney",
+		Region:        "QLD",
+		RegionName:    "Queensland",
+		City:          "South Brisbane",
 		District:      "",
-		ZIP:           "1001",
-		Lat:           &lat2,
-		Lon:           &lon2,
-		Timezone:      "Australia/Sydney",
-		Currency:      "",
-		ISP:           "Cloudflare, Inc.",
-		Org:           "",
+		ZIP:           "4101",
+		Lat:           &cloudflareAllFieldsDefaultLangRequestPointers.lat,
+		Lon:           &cloudflareAllFieldsDefaultLangRequestPointers.lon,
+		Timezone:      "Australia/Brisbane",
+		Offset:        &cloudflareAllFieldsDefaultLangRequestPointers.offset,
+		Currency:      "AUD",
+		ISP:           "Cloudflare, Inc",
+		Org:           "APNIC and Cloudflare DNS Resolver project",
 		AS:            "AS13335 Cloudflare, Inc.",
 		ASName:        "CLOUDFLARENET",
-		Reverse:       "",
-		Mobile:        &mobile2,
-		Proxy:         &proxy2,
-		Hosting:       &hosting2,
+		Reverse:       "one.one.one.one",
+		Mobile:        &cloudflareAllFieldsDefaultLangRequestPointers.mobile,
+		Proxy:         &cloudflareAllFieldsDefaultLangRequestPointers.proxy,
+		Hosting:       &cloudflareAllFieldsDefaultLangRequestPointers.hosting,
 		Query:         "1.1.1.1",
-	}
+	},
+		err: nil,
+	},
+}
 
-	var lat3 float32 = 39.0438
-	
-	location3 := Location{
+var googleSpecificFieldRuLangRequestPointers = struct {
+	lat float32
+}{
+	lat: 39.03,
+}
+
+var googleSpecificFieldRuLangRequestTest = struct {
+	query struct {
+		name  string
+		query Query
+	}
+	out struct {
+		Location
+		err error
+	}
+}{
+	query: struct {
+		name  string
+		query Query
+	}{
+		name: "googleSpecificFieldRuLang",
+		query: Query{
+			Queries: []QueryIP{
+				{Query: "8.8.4.4"},
+			},
+			Fields: "status,message,continent,country,region,city,zip,lat",
+			Lang:   "ru",
+		},
+	},
+	out: struct {
+		Location
+		err error
+	}{Location: Location{
 		Status:        "success",
 		Message:       "",
 		Continent:     "Северная Америка",
@@ -126,9 +197,10 @@ func getSuccessfulBatchResponse() string {
 		City:          "Ашберн",
 		District:      "",
 		ZIP:           "20149",
-		Lat:           &lat3,
+		Lat:           &googleSpecificFieldRuLangRequestPointers.lat,
 		Lon:           nil,
 		Timezone:      "",
+		Offset:        nil,
 		Currency:      "",
 		ISP:           "",
 		Org:           "",
@@ -139,80 +211,99 @@ func getSuccessfulBatchResponse() string {
 		Proxy:         nil,
 		Hosting:       nil,
 		Query:         "",
-	}
-
-	locations := []Location{location1,location2,location3}
-
-	result, _ := json.Marshal(locations)
-
-	return string(result)
+	},
+		err: nil,
+	},
 }
 
-func getSuccessfulFieldListString() string {
-	return "fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,asname,reverse,mobile,proxy,query,hosting"
-}
+var singleQuery = googleAllFieldsDefaultLangRequestTest
 
 func TestSingleQuery(t *testing.T) {
-	var singleQuery = Query{
-		Queries: []QueryIP{
-			{Query:"8.8.8.8"},
-		},
-		Fields:  "status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,asname,reverse,mobile,proxy,query,hosting",
-		Lang:    "",
-	}
+	t.Run(singleQuery.query.name, func(t *testing.T) {
+		location, err := SingleQuery(singleQuery.query.query, "", "", true)
 
-	var location *Location
+		if err != nil && (err.Error() != singleQuery.out.err.Error()) {
+			t.Errorf("got: %#v, want: %#v", err, singleQuery.out.err)
+		}
 
-	location, err := SingleQuery(singleQuery,"","",true)
+		jsonLocation, _ := json.Marshal(location)
+		jsonTestLocation, _ := json.Marshal(singleQuery.out.Location)
 
-	if err != nil {
-		t.Error(err)
-	}
-
-	jsonLocation, _ := json.Marshal(location)
-
-	if string(jsonLocation) != getSuccessfulSingleResponse() {
-		log.Println(string(jsonLocation))
-		log.Println(getSuccessfulSingleResponse())
-		t.Error("Locations did not match")
-	}
+		if string(jsonLocation) != string(jsonTestLocation) {
+			t.Errorf("got: %#v, want: %#v", string(jsonLocation), string(jsonTestLocation))
+		}
+	})
 }
 
+var batchQuery = struct {
+	query Query
+	out   struct {
+		locations []Location
+		err       error
+	}
+}{
+	query: Query{
+		Queries: []QueryIP{
+			{Query: googleAllFieldsDefaultLangRequestTest.query.query.Queries[0].Query},
+			{Query: cloudflareAllFieldsDefaultLangRequestTest.query.query.Queries[0].Query},
+			{Query: googleSpecificFieldRuLangRequestTest.query.query.Queries[0].Query, Fields: googleSpecificFieldRuLangRequestTest.query.query.Fields, Lang: googleSpecificFieldRuLangRequestTest.query.query.Lang},
+		},
+		Fields: allFieldCsv,
+		Lang:   "",
+	},
+	out: struct {
+		locations []Location
+		err       error
+	}{locations: []Location{
+		googleAllFieldsDefaultLangRequestTest.out.Location,
+		cloudflareAllFieldsDefaultLangRequestTest.out.Location,
+		googleSpecificFieldRuLangRequestTest.out.Location,
+	}, err: nil},
+}
 
 func TestBatchQuery(t *testing.T) {
-	var batchQuery = Query{
-		Queries: []QueryIP{
-			{Query:"8.8.8.8"},
-			{Query:"1.1.1.1"},
-			{Query:"8.8.4.4",Fields:"status,message,continent,country,region,city,zip,lat",Lang:"ru"},
-		},
-		Fields:  "status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,asname,reverse,mobile,proxy,query,hosting",
-		Lang:    "",
-	}
+	t.Run("batchQuery", func(t *testing.T) {
+		locations, err := BatchQuery(batchQuery.query, "", "", true)
 
-	var locations []Location
+		if err != nil && (err.Error() != batchQuery.out.err.Error()) {
+			t.Errorf("got: %#v, want: %#v", err, batchQuery.out.err)
+		}
 
-	locations, err := BatchQuery(batchQuery,"","",true)
+		jsonLocations, _ := json.Marshal(locations)
 
-	if err != nil {
-		t.Error(err)
-	}
+		// This is somewhat of a hack
+		//   batch doesn't support reverse field, and to reduce duplicate test data
+		//   we just make the test data for this field an empty string for testing
+		for i := range batchQuery.out.locations {
+			batchQuery.out.locations[i].Reverse = ""
+		}
 
-	jsonLocations, _ := json.Marshal(locations)
+		jsonTestLocations, _ := json.Marshal(batchQuery.out.locations)
 
-	if string(jsonLocations) != getSuccessfulBatchResponse() {
-		log.Println(string(jsonLocations))
-		log.Println(getSuccessfulBatchResponse())
-		t.Error("Locations did not match")
-	}
+		if string(jsonLocations) != string(jsonTestLocations) {
+			t.Errorf("got: %#v, want: %#v", string(jsonLocations), string(jsonTestLocations))
+		}
+	})
+}
+
+var buildFieldListTests = []struct {
+	in  string
+	out string
+}{
+	{
+		in:  allFieldCsv,
+		out: "fields=" + allFieldCsv,
+	},
 }
 
 func TestBuildFieldList(t *testing.T) {
-	fieldsList := "status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,asname,reverse,mobile,proxy,query,hosting"
+	for _, tt := range buildFieldListTests {
+		t.Run(tt.in, func(t *testing.T) {
+			s := buildFieldList(tt.in)
 
-	fieldListString := buildFieldList(fieldsList)
-
-	if fieldListString != getSuccessfulFieldListString() {
-		t.Error("Field list does not match")
+			if s != tt.out {
+				t.Errorf("got %q, want %q", s, tt.out)
+			}
+		})
 	}
 }
